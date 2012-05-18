010af0d5cf0c5fcf981e527cf578603a03ebab53
justin smith <noisesmith@gmail.com>
2012-05-17 20:10:09 -0700

    tidied up xml, replaced copy/pasted svg structure from inkscape with hand written, fixed firefox rendering"

--- src/demo/core.clj
+++ src/demo/core.clj
@@ -16,6 +16,7 @@ div.multicolumn8 {
 	[-moz- | -webkit-]column-gap: 20px;
         -moz-column-count: 8;
         -webkit-column-count: 8;
+
 }
 ")
 
@@ -34,61 +35,41 @@ div.multicolumn8 {
 	    (apply str (map name-to-url (sort scale-files)))
 	    footer)))
 
-(def diagram-start
-;"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>
-"<svg id=\"fingering\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 744.09 1052.4\" version=\"1.1\" xmlns:cc=\"http://creativecommons.org/ns#\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\">
- <defs id=\"defs4\"></defs>
- <metadata id=\"metadata7\">
-  <rdf:RDF>
-   <cc:Work rdf:about=\"\">
-    <dc:format>image/svg+xml</dc:format>
-    <dc:type rdf:resource=\"http://purl.org/dc/dcmitype/StillImage\"/>
-    <dc:title/>
-   </cc:Work>
-  </rdf:RDF>
- </metadata>
- <g id=\"layer1\">
-  <rect id=\"string0\" stroke-linejoin=\"miter\" style=\"stroke-dasharray:none;\" stroke-dashoffset=\"0\" height=\"922.86\" width=\"39.653\" stroke=\"#FFF\" stroke-linecap=\"round\" stroke-miterlimit=\"4\" y=\"22.076\" x=\"174\" stroke-width=\"2.1\" fill=\"#000\"/>
-  <rect id=\"string1\" stroke-linejoin=\"miter\" style=\"stroke-dasharray:none;\" stroke-dashoffset=\"0\" height=\"922.86\" width=\"32.218\" stroke=\"#FFF\" stroke-linecap=\"round\" stroke-miterlimit=\"4\" y=\"22.076\" x=\"344.38\" stroke-width=\"2.1\" fill=\"#000\"/>
-  <rect id=\"string3\" stroke-linejoin=\"miter\" style=\"stroke-dasharray:none;\" stroke-dashoffset=\"0\" height=\"922.86\" width=\"9.9132\" stroke=\"#FFF\" stroke-linecap=\"round\" stroke-miterlimit=\"4\" y=\"22.076\" x=\"685.15\" stroke-width=\"2.1\" fill=\"#000\"/>
-  <rect id=\"string2\" stroke-linejoin=\"miter\" style=\"stroke-dasharray:none;\" stroke-dashoffset=\"0\" height=\"922.86\" width=\"18.587\" stroke=\"#FFF\" stroke-linecap=\"round\" stroke-miterlimit=\"4\" y=\"22.076\" x=\"514.77\" stroke-width=\"2.1\" fill=\"#000\"/>
-  <path id=\"oct0\" stroke-linejoin=\"miter\" style=\"stroke-dasharray:none;\" d=\"m107.14,463.79a19.286,20,0,1,1,-38.571,0,19.286,20,0,1,1,38.571,0z\" transform=\"matrix(0.86740331,0,0,0.88980716,77.32597,70.82072)\" stroke-dashoffset=\"0\" stroke=\"#FFF\" stroke-linecap=\"round\" stroke-miterlimit=\"4\" stroke-width=\"2.39034605\" fill=\"#5c5c5c\"/>
-  <path id=\"oct3\" stroke-linejoin=\"miter\" style=\"stroke-dasharray:none;\" d=\"m107.14,463.79a19.286,20,0,1,1,-38.571,0,19.286,20,0,1,1,38.571,0z\" transform=\"matrix(0.86740331,0,0,0.88980716,591.77822,70.82072)\" stroke-dashoffset=\"0\" stroke=\"#FFF\" stroke-linecap=\"round\" stroke-miterlimit=\"4\" stroke-width=\"2.39034605\" fill=\"#5c5c5c\"/>
-  <path id=\"oct2\" stroke-linejoin=\"miter\" style=\"stroke-dasharray:none;\" d=\"m107.14,463.79a19.286,20,0,1,1,-38.571,0,19.286,20,0,1,1,38.571,0z\" transform=\"matrix(0.86740331,0,0,0.88980716,416.21074,70.82072)\" stroke-dashoffset=\"0\" stroke=\"#FFF\" stroke-linecap=\"round\" stroke-miterlimit=\"4\" stroke-width=\"2.39034605\" fill=\"#5c5c5c\"/>
-  <path id=\"oct1\" stroke-linejoin=\"miter\" style=\"stroke-dasharray:none;\" d=\"m107.14,463.79a19.286,20,0,1,1,-38.571,0,19.286,20,0,1,1,38.571,0z\" transform=\"matrix(0.86740331,0,0,0.88980716,248.55643,70.82072)\" stroke-dashoffset=\"0\" stroke=\"#FFF\" stroke-linecap=\"round\" stroke-miterlimit=\"4\" stroke-width=\"2.39034605\" fill=\"#5c5c5c\"/>
-  <text id=\"octavelable\" font-weight=\"normal\" xml:space=\"preserve\" font-size=\"40px\" font-style=\"normal\" font-stretch=\"normal\" font-variant=\"normal\" y=\"494.80505\" x=\"7.7142868\" font-family=\"Bitstream Charter\" fill=\"#000000\"><tspan id=\"tspan2902\" x=\"7.7142868\" y=\"494.80505\">octave</tspan></text>
-<text id=\"dominantlabel\" font-weight=\"normal\" xml:space=\"preserve\" font-size=\"40px\" font-style=\"normal\" font-stretch=\"normal\" font-variant=\"normal\" y=\"778\" x=\"7.7142868\" font-family=\"Bitstream Charter\" fill=\"#000000\"><tspan id=\"tspan2902\" x=\"7.7142868\" y=\"340\">fifth</tspan></text>
-<text id=\"octave2label\" font-weight=\"normal\" xml:space=\"preserve\" font-size=\"40px\" font-style=\"normal\" font-stretch=\"normal\" font-variant=\"normal\" y=\"778\" x=\"7.7142868\" font-family=\"Bitstream Charter\" fill=\"#000000\"><tspan id=\"tspan2902\" x=\"7.7142868\" y=\"728\">octv. 2</tspan></text>")
-
-(def diagram-end
- "</g>
-</svg>")
-
-
-(defn get-pos [coords]
-  (str
-   "\n<path id=\"position\" stroke-linejoin=\"miter\" style=\"stroke-dasharray:none;\" fill=\"#982020\" stroke-dashoffset=\"0\" transform=\"translate("
-   (+ (nth coords 0) 342.85714)
-   ","
-   (- (nth coords 1) 303.71429)
-   ")\" stroke=\"#FFF\" stroke-linecap=\"round\" stroke-miterlimit=\"4\" stroke-width=\"2.0999999\" d=\"m-91.429,350.93-37.843-21.008,37.115-22.269,0.72803,43.277z\"/>\n"))
-
-; diagram working in chrome but not firefox
 (defn diagram [positions]
-  (str diagram-start
-       (apply str (map get-pos positions))
-       diagram-end))
+  (let [svg-rect (fn [x0 y0 x1 y1 color]
+		     (str "<path d=\"M" x0 " " y0 " L" x1 " " y0 " L" x1 " "
+			  y1 " L" x0 " " y1 "Z\" fill=\"" color "\"/>"))
+	svg-dot (fn [x y r c]
+		    (str "<circle cx=\"" x "\" cy=\"" y "\" r=\"" r
+			 "\" stroke=\"black\""
+			 " stroke-width=\"2\" fill=\"" c "\"/>"))
+	svg-arrow (fn [x y c]
+		      (str "<path d=\"M0 0 L10 -5 L10 5 L0 0 Z\" fill=\"" c
+			   "\" stroke=\"black\""
+			   " transform=\"" "translate(" x "," y ")\"/>"))]
+     (str "<div float=\"right\" margin=\"0\" padding=\"0\"><svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" viewBox=\"0 0 600 800\">"
+	  ; strings
+	  (svg-rect 35 10 53 600 "black")
+	  (svg-rect 75 10 89 600 "black")
+	  (svg-rect 115 10 126 600 "black")
+	  (svg-rect 155 10 163 600 "black")
+	  (apply str (map (fn [x] (svg-arrow (nth x 0) (+ 10 (nth x 1)) "red"))
+			  positions))
+	  (svg-dot 25 308 8 "gray")
+	  (svg-dot 25 456 6 "gray")
+	  (svg-dot 25 206 6 "gray") ;fifth
+	  "</svg></div>\n")))
 
 (def fundamental 130.813)
 
 (def cents-base (Math/pow 2 (/ 1 1200)))
 
 (defn all-spots [freqs]
-  (let [string-data [[fundamental 1]
-		     [(* fundamental (Math/pow cents-base 700)) 163]
-		     [(* fundamental (Math/pow cents-base 1400)) 320]
-		     [(* fundamental (Math/pow cents-base 2100)) 482]]
-	string-scale 918
+  (let [string-data [[fundamental 55]
+		     [(* fundamental (Math/pow cents-base 700)) 91]
+		     [(* fundamental (Math/pow cents-base 1400)) 127]
+		     [(* fundamental (Math/pow cents-base 2100)) 165]]
+	string-scale 600
 	coord (fn [hz hz-pos]
 		  (let [base-hz (nth hz-pos 0)
 			pos (nth hz-pos 1)]
@@ -172,24 +153,29 @@ div.multicolumn8 {
 (defn process-scale-request [req]
   (let [scale-request (str/split req #":")
        name (nth scale-request 0)
-       header (str "<html><head><title>" name "</title><link href=\"scales.css\" rel=\"stylesheet\"type=\"text/css\"></head><body>")
+       header (str "<html xmlns='http://www.w3.org/1999/xhtml'><head>
+<title>" name "</title><link href=\"scales.css\" rel=\"stylesheet\" type=\"text/css\"></link></head><body>")
        footer (str "</div></body></html>")
        scale-text (try (slurp (str "scl/" name ".scl"))
 		       (catch Exception e
-			      (str "failed in opening file: scl/" name "<br>"
+			      (str "failed in opening file: scl/" name "<br/>"
 				   (.getMessage e))))
        lines (str/split-lines scale-text)
        base 130.813
        mults (sort (lines-rec lines))]
-    (str header "<h1>" name "</h1><br><div class=\"multicolumn2\"><pre>"
-	 scale-text
-	 "</pre>"
-	 "<table border=\"1\"><tr>"
-	 "<th>mult</th><th>cents</th><th>note</th><th>hz</th></tr>"
-	 (apply str (show-info mults))
-	 "</table>"
-	 (diagram (all-spots (freqs-hz base (sort > mults))))
-	 footer)))
+    {:status 200
+     :headers {"Content-Type" "application/xhtml+xml"}
+     :body
+     (str header "<h1>" name "</h1><br/><div class=\"multicolumn2\">"
+	  "<div float=\"left\" margin=\"0\" padding=\"0\"><pre>"
+	  scale-text
+	  "</pre>"
+	  "<table border=\"1\"><tr>"
+	  "<th>mult</th><th>cents</th><th>note</th><th>hz</th></tr>"
+	  (apply str (show-info mults))
+	  "</table></div>"
+	  (diagram (all-spots (freqs-hz base (sort > mults))))
+	  footer)}))
 
 (def scl-memo (memoize list-scales))
 
@@ -199,7 +185,7 @@ div.multicolumn8 {
   (GET "/scales.css" [] stylesheet)
   (route/resources "/")
   (GET "/showscale*" {params :query-params}
-       (process-scale-request (get params "scale")))
+		  (process-scale-request (get params "scale")))
 ;  (GET "/:req" [req]
 ;       (process-scale-request req))
   (route/not-found "<h1>Page not found</h1>"))
